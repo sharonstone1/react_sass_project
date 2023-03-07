1a3befb5fe974d4c3b9f69ba12fc0c119a53f5fc
martheC <marthe.ekoule@gmail.com>
2023-03-07 16:36:45 +0000

    my first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import classes from "../sassStyles/layouts/Header.module.scss";
+
+import logo from "../images/logo-yellow.jpg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__logo_box">
+        <img src={logo} alt="Logo" className="header__logo" />
+      </div>
+      <div className="header__text_box">
+        <h1 className="heading__primary">
+          <p>
+            <span className="heading__primary_main">Full Stack Developer</span>
+          </p>
+          <p>
+            <span className="heading__primary_sub">
+              Coding is my Life and It is funny when you like it.
+            </span>
+          </p>
+        </h1>
+
+        <a href="#" className="btn__btn_white__btn_animated">
+          Dicover our Coding Tours
+        </a>
+      </div>
+    </header>
+  );
+};
+export default Header;
